274682504c132525d09a83707f6766b73af74c87
Per Pascal Seeland <pascal.seeland@flightmare.net>
2020-05-10 15:33:26 +0200

    Add showing stdout and listing files

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM python:3.6-alpine
 
+LABEL maintainer="pascal.seeland@tik.uni-stuttgart.de"
+
 WORKDIR /
 
 COPY requirements.txt ./
@@ -8,13 +10,14 @@ RUN apk add --no-cache --virtual .build-deps  \
 		gcc \
 		libffi-dev \
  		musl-dev \
-#		ncurses-dev \
 		openssl-dev \
   &&  pip install --no-cache-dir -r requirements.txt \
   &&   apk del --no-network .build-deps 
 
 COPY src app
 COPY input input
+
+EXPOSE 5000
 
 ENV PYHTONPATH app
 ENTRYPOINT [ "python" ]
--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,17 +1,34 @@
 let ws;
+let files;
+
 document.addEventListener("DOMContentLoaded", function(){
     document.querySelectorAll('.partcontent').forEach(function(partcontent){
-        partcontent.value = atob(JSON.parse(atob(document.querySelector('#dataholder').getAttribute('data-template')))['files'][0]['parts'][0]['content']);
+        partcontent.value = Base64.decode(JSON.parse(Base64.decode(document.querySelector('#dataholder').getAttribute('data-template')))['files'][0]['parts'][0]['content']);
     });
     let token = document.querySelector('#dataholder').getAttribute('data-token')
     ws = new WebSocket("ws://localhost:8083/computations")
     ws.onopen = () => {
         ws.send(JSON.stringify({"type":"authenticate","content":{"jwt":token}}));
+        document.getElementById("submit").disabled = false;
     };
+    ws.onmessage = function(event) {
+        data = JSON.parse(event.data)
+        switch  (data.type) {
+            case "computation":
+                displayComputation(data.content);
+                break;
+            case "result":
+                displayResult(data.content);
+                break;
+            default:
+                console.error(data);
+        }
+    }
     document.getElementById("submit").onclick = senddata;
 });
 
 function senddata() { 
+    document.getElementById("submit").disabled = true;
     task = {
         "type":"create-computation",
         "content":{
@@ -30,9 +47,50 @@ function senddata() {
         }
     };
     document.querySelectorAll('.partcontent').forEach(function(partcontent){
-        console.log(partcontent);
+        //console.log(partcontent);
         task['content']['task']['files'][0]['parts'].push({'identifier':partcontent.id, 'content':btoa(partcontent.value)});
     });
+    document.querySelector('#stdout').value = '';
     ws.send(JSON.stringify(task));
+    return false;
+}
+
+function displayComputation(computation) {
+    //console.log(computation)
+}
+
+function displayResult(result) {
+    console.log(result)
+    document.querySelector('#stdout').value += Base64.decode(result.result.output.stdout);
+    if ("final" == result.result.status) {
+        document.getElementById("submit").disabled = false;
+    }
+    files = new Map();
+    result.result.files.forEach(file => {
+        var linode = document.createElement("li");
+        var anode = document.createElement("a");
+        anode.setAttribute('href', '#');
+        anode.addEventListener("click",function() { return save(file.path,file.identifier,file.MIMEtype)});
+        var textnode = document.createTextNode(file.path);
+        anode.appendChild(textnode);
+        linode.appendChild(anode);
+        document.getElementById("fileList").appendChild(linode);
+        files[file.identifier] = Base64.decode(file.content);
+    });
+}
+
+function save(filename, identifier, mimetype) {
+    var blob = new Blob([files[identifier]], {mimetype: mimetype});
+    if(window.navigator.msSaveOrOpenBlob) {
+        window.navigator.msSaveBlob(blob, filename);
+    }
+    else{
+        var elem = window.document.createElement('a');
+        elem.href = window.URL.createObjectURL(blob);
+        elem.download = filename;
+        document.body.appendChild(elem);
+        elem.click();
+        document.body.removeChild(elem);
+    }
     return false;
 }
